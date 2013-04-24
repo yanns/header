1669c43877052592b31675ce6bc8feb38d11b97a
Yann Simon <yann.simon@epost-dev.de>
2013-04-24 08:56:18 +0200

    ArrayIndexOutOfBoundsException occurs

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,6 +5,9 @@ import play.mvc.*;
 public class Application extends Controller {
   
     public static Result index() {
+        if (!request().accepts("application/json")) {
+            return status(NOT_ACCEPTABLE);
+        }
         return ok("Your new application is ready.");
     }
   
